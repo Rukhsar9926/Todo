f4614086cb9c940c6652b8627e8024f4b1d90ebd
Rukhsar9926 <153531256+Rukhsar9926@users.noreply.github.com>
2024-01-22 14:14:11 +0530

    i have add some funtionalty

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,us } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Navbar from './Component/Navbar';
@@ -9,50 +9,84 @@ import ListGroup from './Component/ListGroup';
 
 
 const App = () => {
-  const [todos,setTodos]=useState([
-   
+  const [todos, setTodos] = useState([
+
   ])
 
-const addList=(text)=>{
-    const newLi={
-      id:crypto.randomUUID(),
-      text:text
+  const [edit,setEdit]=useState({
+    item:{},
+    editMode: false
+
+
+
+  })
+
+
+
+  const addList = (text) => {
+    const newLi = {
+      id: crypto.randomUUID(),
+      text: text
 
     }
-    setTodos([newLi,...todos])
-}
+    setTodos([newLi, ...todos])
+  }
 
 
-const deleteList=(id)=>{
-
-    const todoData = todos.filter((item)=>{  if(item.id !==id){
+  const deleteList = (id) => {
+    const todoData = todos.filter((item) => {
+      if (item.id !== id) {
         return item
       }
 
     })
-    console.log(todoData,"todoData")
-    setTodos(todoData)}
-//fghjk
-///fghjkl
-//fghjkl
- 
-  return (
-    
-  <>
-    <Navbar/>
-    <div className='container mt-3'>
-      <Form addList={addList}/>
-      <ListGroup todos={todos} deleteList={deleteList}/>
-
-    </div>
+    console.log(todoData, "todoData")
+    setTodos(todoData)
+  }
 
 
 
-   
-   
-    </>
+  const editList= (item) => {
+    setEdit({
+      item : item,
+      editMode: true
+    })
+    }
+
+
   
-   
+  const updateList =(oldId,newText)=>{
+    setTodos(todos.map((newitem)=>
+    newitem.id==oldId ?{id:oldId,text:newText}:newitem))
+
+    setEdit({
+      item :{},
+      editMode:false
+    })
+  }
+
+
+
+
+
+
+  return (
+
+    <>
+      <Navbar />
+      <div className='container mt-3'>
+        <Form addList={addList} edit={edit} updateList={updateList}/>
+        <ListGroup todos={todos} deleteList={deleteList} editList={editList}  />
+
+      </div>
+
+
+
+
+
+    </>
+
+
 
   )
 }
--- src/Component/ListItem.jsx
+++ src/Component/ListItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ListItem = ({item,deleteList}) => {
+const ListItem = ({item,deleteList,editList}) => {
 
     
 return (
    <>
    <li class="list-group-item" >{item?.text}   
-   <button type="button" class="btn btn-danger float-end"onClick={()=>deleteList(item.id)} >Delete</button>
-   <button type="button" class="btn btn-warning float-end " >Edit</button>
+   <button type="button" class="btn btn-danger float-end"onClick={()=>deleteList(item.id)}>Delete</button>
+   <button type="button" class="btn btn-warning float-end " onClick={()=>editList(item)}>Edit</button>
    </li>
    </>
   )
